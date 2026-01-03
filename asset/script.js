8ff05276164b2ab1f1e80e3812ac4d51c7598191
BrightSitedev <josephtelecom.ng@gmail.com>
2026-01-03 23:59:54 +0100

    Add files via upload

--- asset/script.js
+++ asset/script.js
@@ -549,3 +549,14 @@ if (faqSearch) {
         if (manualInstallContainer) manualInstallContainer.style.display = 'none';
         if (popup) popup.style.display = 'none';
     });
+
+    // ==========================================
+//  REGISTER SERVICE WORKER (For Offline Mode)
+// ==========================================
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+        navigator.serviceWorker.register('/sw.js')
+            .then(reg => console.log('Service Worker Registered!', reg.scope))
+            .catch(err => console.log('Service Worker Failed:', err));
+    });
+}
